967c69bc0c9a42e77c7c630ab37931c2d37ac73d
Greenyello Ninja <joshua.deck@liveoak.bank>
2023-10-24 01:54:38 +0000

    #9 Add random terraform provider

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "random" {
   # Configuration options
 }
 
-resource "random_string" "random" {
+resource "random_string" "bucket_name" {
   length           = 16
   special          = false
 }
 
-output "random_bucket_name_id" {
+output "random_bucket_name_result" {
     value = random_string.bucket_name.result
 }

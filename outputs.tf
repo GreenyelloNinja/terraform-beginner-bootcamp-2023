d294ac6fe7be89df6f47359af65132819745df6c
Greenyello Ninja <joshua.deck@liveoak.bank>
2023-11-29 12:27:09 +0000

    can create tf provider

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-    description = "Bucket Name for our static website hosting"
-    value = module.terrahouse_aws.bucket_name
-  
-}
-output "s3_website_endpoint" {
-    description = "S# Static Website hosting endpoint"
-    value = module.terrahouse_aws.website_endpoint
-}
+//output "bucket_name" {
+//    description = "Bucket Name for our static website hosting"
+//    value = module.terrahouse_aws.bucket_name
+//  
+//}
+//output "s3_website_endpoint" {
+//    description = "S# Static Website hosting endpoint"
+//    value = module.terrahouse_aws.website_endpoint
+//}
 
-output "cloudfront_url" {
-    description = "The CloudFront Distro domain name"
-    value = module.terrahouse_aws.cloudfront_url
-}
+//output "cloudfront_url" {
+//    description = "The CloudFront Distro domain name"
+//    value = module.terrahouse_aws.cloudfront_url
+//}
